4580871589aeea9bad9f96e7a9d581a2288d9650
triandikaabriansyah <triandikaabriansyah@gmail.com>
2021-05-21 16:13:25 +0700

    Mengubah alert hapus dengan sweetalert

--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -59,7 +59,7 @@
 				    <p class="card-text">Penerbit : {{$buku->penerbit}}</p>
 				    <a href="/buku/{{$buku->id_buku}}/edit" class="btn btn-warning mr-2">Edit</a>
 					
-				    <a href="/buku/{{$buku->id_buku}}/hapus" class="btn btn-danger" onclick="return confirm('Yakin mau dihapus')">Hapus</a>
+				    <a href="/buku/{{$buku->id_buku}}/hapus" class="btn btn-danger tombol-hapus">Hapus</a>
 				  </div>
 				</div>	
     		</div>  
@@ -73,5 +73,10 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+	<!-- Sweet alert -->
+	<script src="/js/sweetalert2.all.min.js"></script>
+	<!-- My js -->
+	<script src="/js/script.js"></script>
   </body>
 </html>
